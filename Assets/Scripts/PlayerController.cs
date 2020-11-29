b0c92cf2d367b42a214567c7f14a1bba645319f1
AndyBogle <50696217+AndyBogle@users.noreply.github.com>
2020-11-29 17:15:47 +0000

    Alpha upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    public float jumpForce = 10;
+    public float gravityModifier = 1;
+    public bool isOnGround = true;
+    public bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Jump wen pressing space
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver != true)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
+
+            isOnGround = false;
+        }
+     
+        if (playerRb.transform.position.y <= -22.8f)
+        {
+            GameOver();
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+        gameOver = true;
+
+        playerAnim.SetBool("Death_b", true);
+        playerAnim.SetInteger("DeathType_int", 2);
+        SceneManager.LoadScene("StartMenu");
+
+    }
+}
---
+++ Assets/Scripts/RepeatGround.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatGround : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatGWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatGWidth = GetComponent<BoxCollider>().size.x * 26;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 1; i >= 5; i++)
+        {
+            if(i >= 5 && transform.position.x > startPos.x - repeatGWidth)
+            {
+                //transform.position = startPos;
+                startPos.x = + 5;
+            }
+        }
+
+        if (transform.position.x < startPos.x - repeatGWidth)
+        {
+            transform.position = startPos;
+        }
+        
+    }
+}
